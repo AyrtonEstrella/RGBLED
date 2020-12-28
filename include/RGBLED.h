0ef89d2e787e103c625e1e2fd2d8f51ca43a683f
Ayrton Estrella <ayrtonestrella@outlook.com>
2020-12-28 15:07:22 -0600

    Adapt library for ESP32

--- include/RGBLED.h
+++ include/RGBLED.h
@@ -4,11 +4,35 @@
 /************************* ADDITIONAL LIBRARIES **********************/
 #include <Arduino.h>
 
+/************************* LIBRARY OPTIONS **********************/
+
+#define LED_ON_IS_LOW       // Comment this line if LED turns off when control pin is LOW
+// #define DEBUG_LED_COLOR    // Uncomment to print the RGB hex codes
+
 /************************* CONSTANTS **********************/
+#ifdef LED_ON_IS_LOW
+  #define LED_ON  LOW
+  #define LED_OFF HIGH
+#else 
+  #define LED_ON  HIGH
+  #define LED_OFF LOW
+#endif
 
+#define LED_VERY_FAST_TRANSITION_MS 1
+#define LED_FAST_TRANSITION_MS      3
+#define LED_SLOW_TRANSITION_MS      10
 
+#define PWM_RANGE_VALUE 255
+#define BLACK_COLOR   0x000000
+#define RED_COLOR     0xFF0000
+#define GREEN_COLOR   0x00FF00
+#define BLUE_COLOR    0x0000FF
+#define YELLOW_COLOR  0xFFFF00
+#define CYAN_COLOR    0x00FFFF
+#define MAGENTA_COLOR 0xFF00FF
+#define ORANGE_COLOR  0xFFA500
+#define WHITE_COLOR   0xFFFFFF
 /************************* VARIABLES **********************/
-
 
 /************************* MACROS **********************/
 
@@ -17,10 +41,28 @@
 
 class RGBLED {
   private:
+    uint8_t _redValue = 0, _toRedValue = 0, _redBrightnessValue = 0;
+    uint8_t _greenValue = 0, _toGreenValue = 0, _greenBrightnessValue = 0;
+    uint8_t _blueValue = 0, _toBlueValue = 0, _blueBrightnessValue = 0;
+    uint32_t _currentColor = 0, _targetColor = 0;
+    uint8_t _brightness = 25;
 
+    uint16_t _pwmFrequency = 5000;
+    uint8_t _redChannel = 0; _greenChannel = 1; _blueChannel = 2;
+    uint8_t _pwmResolution = 8,
+    uint16_t _redPin = 0, _greenPin = 0, _bluePin = 0; 
+
+    void UpdateAnimation();
+    void UpdateColor();
+    void ApplyBrightness();
   public:
-    TCPManager();
+    RGBLED(uint16_t redPin, uint16_t greenPin, uint16_t bluePin);
 
+    uint32_t GetCurrentColor();
+    void SetBrightness(uint8_t brightness);
+    void SetColor(uint32_t color);
+    void ColorTransition(uint32_t color, uint8_t transitionDelay = LED_FAST_TRANSITION_MS);
+    void Demo();
 };
 
 #endif
--- src/RGBLED.cpp
+++ src/RGBLED.cpp
@@ -1,6 +1,136 @@
 #include <Arduino.h>
 #include "RGBLED.h"
 
-RGBLED::RGBLED() {
-  
+RGBLED::RGBLED(uint16_t redPin, uint16_t greenPin, uint16_t bluePin) {
+  _redPin = redPin;
+  _greenPin = greenPin;
+  _bluePin = bluePin;
+
+  ledcSetup(_redChannel, _pwmFrequency, _pwmResolution);
+  ledcSetup(_greenChannel, _pwmFrequency, _pwmResolution);
+  ledcSetup(_blueChannel, _pwmFrequency, _pwmResolution);
+
+  ledcAttachPin(_redPin, _redChannel);
+  ledcAttachPin(_greenPin, _greenChannel);
+  ledcAttachPin(_bluePin, _blueChannel);
+}
+
+/**
+  @brief  Update the LED transition animation. This function is responsible for the
+          LED smooth transition effect.
+*/
+void RGBLED::UpdateAnimation() {
+  if (__redValue != _toRedValue || _greenValue != _toGreenValue || _blueValue != _toBlueValue) {
+    if (_redValue < _toRedValue)
+      _redValue += 1;
+    else if (_redValue > _toRedValue)
+      _redValue -= 1;
+
+    if (_greenValue < _toGreenValue)
+      _greenValue += 1;
+    else if (_greenValue > _toGreenValue)
+      _greenValue -= 1;
+
+    if (_blueValue < _toBlueValue)
+      _blueValue += 1;
+    else if (_blueValue > _toBlueValue)
+      _blueValue -= 1;
+  }
+
+  UpdateColor();
+}
+
+/**
+  @brief  Update the PWM values for the current color values
+*/
+void RGBLED::UpdateColor() {
+  _currentColor = (_redValue << 16) + (_greenValue << 8) + _blueValue;
+
+  ApplyBrightness();  
+
+  #ifdef LED_INVERT_CONTROL_SIGNAL
+    ledcWrite(_redChannel, PWM_RANGE_VALUE - _redValue);
+    ledcWrite(_greenChannel, PWM_RANGE_VALUE - _greenValue);
+    ledcWrite(_blueChannel, PWM_RANGE_VALUE - _blueValue);
+  #else
+    ledcWrite(_redChannel, _redValue);
+    ledcWrite(_greenChannel, _greenValue);
+    ledcWrite(_blueChannel, _blueValue);
+  #endif
+}
+
+/**
+  @brief  Get the current color in RGB hex code 
+  @return The current LED color
+*/
+uint32_t RGBLED::GetCurrentColor() {
+  return _currentColor;
+}
+
+/**
+  @brief  Apply the brigthness factor to the PWM value
+*/
+void RGBLED::ApplyBrightness() {
+  _redBrightnessValue = (_redValue * _brightness / 100);
+  _greenBrightnessValue = (_greenValue * _brightness / 100);
+  _blueBrightnessValue = (_blueValue * _brightness / 100);
+}
+
+/**
+  @brief  Set the desired _brightness 
+  @param _brightness Desired _brightness for all colors
+*/
+void RGBLED::SetBrightness(uint8_t brightness) {
+  _brightness = brightness;
+}
+
+/**
+  @brief  Receive a 3 byte value containing the RGB combination.
+          Examples: RED_COLOR (0xFF0000), GREEN_COLOR (0x00FF00), BLUE_COLOR (0x0000FF)
+  @param color The 32-bit color in RGB mode
+*/
+void RGBLED::SetColor(uint32_t color) {
+  _redValue = lowByte(color >> 16);
+  _greenValue = lowByte(color >> 8);
+  _blueValue = lowByte(color);
+
+  #ifdef DEBUG_LED_COLOR
+    Serial.println("Color: " + String(color, HEX));
+    Serial.println("red: " + String(_redValue, HEX));
+    Serial.println("green: " + String(_greenValue, HEX));
+    Serial.println("blue: " + String(_blueValue, HEX));
+  #endif
+
+  UpdateColor();
+}
+
+/**
+  @brief  Set the new color with a transition effect.
+  @param color The 32-bit color in RGB mode
+  @param transitionDelay Optional parammeter to set the transition delay
+*/
+void RGBLED::ColorTransition(uint32_t color, uint8_t transitionDelay) {
+  _toRedValue = lowByte(color >> 16);
+  _toGreenValue = lowByte(color >> 8);
+  _toBlueValue = lowByte(color);
+
+  while (_redValue != _toRedValue || _greenValue != _toGreenValue || _blueValue != _toBlueValue) {
+    UpdateAnimation();
+    delay(transitionDelay);
+  }
+}
+
+/**
+  @brief  LED demo animation
+*/
+void RGBLED::Demo() {
+  ColorTransition(RED_COLOR);
+  ColorTransition(GREEN_COLOR);
+  ColorTransition(BLUE_COLOR);
+
+  delay(1000);
+
+  ColorTransition(RED_COLOR, LED_SLOW_TRANSITION_MS);
+  ColorTransition(GREEN_COLOR, LED_SLOW_TRANSITION_MS);
+  ColorTransition(BLUE_COLOR, LED_SLOW_TRANSITION_MS);
 }
